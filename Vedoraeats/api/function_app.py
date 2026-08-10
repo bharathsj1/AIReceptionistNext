9469d99b9a5477b4dd6c7f99dda2a9b402e421a8
Bharath <bharath.sj1@gmail.com>
2026-08-10 04:07:07 +0100

    Remove Vedoraeats standalone project from monorepo

--- Vedoraeats/api/function_app.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-
-from shared.db import init_db
-
-init_db()
-
-app = func.FunctionApp()
-
-import vedora_endpoints  # noqa
--- Vedoraeats/src/App.jsx
+++
@@ -1,5 +0,0 @@
-import VedoraLanding from "./components/vedora/VedoraLanding";
-
-export default function App() {
-  return <VedoraLanding />;
-}
